c2dabec2ffc8f2c256e6ffbca18b87806126a2ab
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-10-26 15:52:29 -0300

    Feito até o exercício 8

--- app/router/routes.py
+++ app/router/routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
+from typing import Optional
+
 
 
 router = APIRouter()
@@ -12,10 +15,36 @@ async def read_root():
 async def read_status():
     return {"status": "Servidor funcionando"}
 
-class UserResponse(BaseModel):
+class ResponseModelUser(BaseModel):
     username: str
     message: str
 
-@router.get("/user/{username}", response_model=UserResponse)
-async def get_user(username: str):
-    return UserResponse(username= username, message= f"Bom dia {username}")
+
+bd_users = {
+    "Rodrigo": {"username": "Rodrigo", "age": 32},
+    "Jonatas": {"username": "Jonatas", "age": 33},
+}
+
+@router.get("/consult-user/{username}", response_model=ResponseModelUser)
+async def consult_user(username: str):
+    if username not in bd_users:
+        raise HTTPException(status_code=404, detail="Usuário não cadastrado")
+    return ResponseModelUser(username= username, message= f"Bom dia {username}")
+
+
+
+
+class ReceiveModelUser(BaseModel):
+    username: Annotated[str, Field(min_length=1)] 
+    age: Annotated[int, Field(gt=0)] 
+
+
+@router.post("/create-user")
+async def create_user(user: ReceiveModelUser):
+    bd_users[user.username] = {"username": user.username, "age": user.age}
+    
+    return ResponseModelUser(username= user.username, message= "Cadastro Realizado com Sucesso")
+
+
+
+
